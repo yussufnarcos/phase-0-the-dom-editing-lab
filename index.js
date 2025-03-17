5b1cbc9f69bb643bad7fef62802414a2ac0b598f
yussufnarcos <narcosyussuf@gmail.com>
2025-03-17 14:26:54 +0300

    phase-0-the-dom-editing-lab

---
+++ index.js
@@ -0,0 +1,15 @@
+before(() => {
+    document.body.innerHTML = `
+      <h1>Welcome</h1>
+      <p>
+        This is a <strong>strong</strong> and <em>emphasized</em> text example.
+        Learn more about HTML <a href="https://developer.mozilla.org/en-US/docs/Web/HTML">here</a>.
+      </p>
+      <table>
+        <tr>
+          <td>Sample</td>
+        </tr>
+      </table>
+    `;
+  });
+  
